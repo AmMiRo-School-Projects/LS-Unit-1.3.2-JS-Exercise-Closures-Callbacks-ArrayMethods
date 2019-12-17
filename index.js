193d772f5cac5f9ed10b4f5e44b5e1d3bbc61347
Amos Rose <amosroseacct@gmail.com>
2019-12-16 21:55:19 -0700

    6th challenge complete

--- index.js
+++ index.js
@@ -177,8 +177,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
  */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  const fullName = [];
+  runners.forEach(function(ele) {
+    fullName.push(`${ele.last_name}, ${ele.first_name}`);
+  });
+  return fullName;
 }
 
 /**
@@ -193,8 +197,10 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
  */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map(function(runner) {
+    runner.first_name.touppercase();
+  });
 }
 
 /**
